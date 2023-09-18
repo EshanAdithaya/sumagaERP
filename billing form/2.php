49b098fa41f802e2e4a446d26ebe3a17e720205d
Eshan Adithaya <87906517+EshanAdithaya@users.noreply.github.com>
2023-09-18 10:40:52 +0530

    necessary edits

--- billing form/2.php
+++ billing form/2.php
@@ -1,13 +1,12 @@
 <?php
 $con = mysqli_connect('127.0.0.1','root','');
 
-if(!$con)
-{
-echo 'not connected to the server';
+if(!$con) {
+    echo 'not connected to the server';
 }
-if(!mysqli_select_db($con,'sumaga_batagama'))
-{
-echo 'database not selected';
+
+if(!mysqli_select_db($con,'sumaga_batagama')) {
+    echo 'database not selected';
 }
 
 $student_name = $_POST['name'];
@@ -15,15 +14,13 @@ $student_ID = $_POST['ID'];
 $amount = $_POST['amount'];
 $month = $_POST['month'];
 $grade = $_POST['grade'];
-$payment_date = $_POST['date'];
 
-$sql = "INSERT INTO payments(`student_name`, `student_ID`, `amount`, `month`, `grade`, `payment_date`) VALUES ('$student_name','$student_ID','$amount','$month','$grade','$payment_date')";
- if(!mysqli_query($con,$sql))
- {
- echo '<h1>not inserted</h1>';
- }
- else
- {
- echo '<h1>inserted</h1>';
- }
+$sql = "INSERT INTO payments(`student_name`, `student_ID`, `amount`, `month`, `grade`) 
+        VALUES ('$student_name','$student_ID','$amount','$month','$grade')";
+
+if(!mysqli_query($con,$sql)) {
+    echo '<h1>not inserted</h1>';
+} else {
+    echo '<h1>inserted</h1>';
+}
 ?>
